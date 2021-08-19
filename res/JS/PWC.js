e626b512a56fe3b76672051fd54aa4c703e12544
SYH08 <yihyunsong@outlook.com>
2021-08-19 18:37:53 +0900

    creat2

---
+++ res/JS/PWC.js
@@ -0,0 +1,10 @@
+password = prompt("Authentication");
+
+    if(password !='G1C7FA Beta')
+    {
+
+      alert(':( You do not have permission to view this site');
+      window.location.href='https://hunminjeungum.github.io/G1C2FA/Sub%20HTML/50rry.html';
+
+
+    }
